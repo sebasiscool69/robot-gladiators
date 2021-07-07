b8bd0c6e24eaee585a1b1d6ea9d261ca6d6b143c
sebz <bonillas175@gmail.com>
2021-07-06 22:50:14 -0400

    fight adjustment

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,22 +10,10 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
-  window.alert("Welcome to Robot Gladiators!");
-};
-
-var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
-  
-    //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-  
-    // Log a resulting message to the console so we know that it worked.
-  
-    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-  
-    // Log a resulting message to the console so we know that it worked.
-  };
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+      //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
 enemyHealth = enemyHealth - playerAttack;
 
 // Log a resulting message to the console so we know that it worked.
@@ -61,3 +49,5 @@ if (playerHealth <= 0) {
 else {
   window.alert(playerName + " still has " + playerHealth + " health left.");
 }
+  };
+fight();
